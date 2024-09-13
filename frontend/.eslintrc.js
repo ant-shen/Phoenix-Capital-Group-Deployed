f5657eac334dd51ca533b0570e7cb63380e85de3
ant-shen <anthony.shen11@gmail.com>
2024-09-12 17:41:31 -0700

    Update .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      'plugin:vue/essential',
-      'eslint:recommended',
-    ],
-    parserOptions: {
-      parser: 'babel-eslint',
-    },
-    rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
-  };
-  
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  globals: {
+    process: 'readonly',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
